f72666a8b5d5d6b69e3c20dd87a0b331dd0f5784
artemanistratov-yolo <artem.anistratov@yolo.com>
2025-09-09 08:09:46 -0400

    Added state managment

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,12 +5,11 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Artems Garden Shop, where green meets serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        At Artems Garden Shop, we are passionate about bringing nature closer to you.
+         Our mission is to offer a wide variety of high-quality plants that not only beautify your surroundings but also support a healthier and more sustainable lifestyle. 
+         From air-purifying greenery to wonderfully fragrant plants, we have something special for every plant lover.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
@@ -22,7 +21,7 @@ function AboutUs() {
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
+        Join us in our mission to create a greener, healthier world. Visit Artems Garden Shop today and experience the 
         beauty of nature right at your doorstep.
       </p>
     </div>
